2d15a168723882d4f2b4e374941272ce5bc958b9
Dima Solovey <38838582+disolovei@users.noreply.github.com>
2019-10-23 22:09:01 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,44 @@
 'use strict';
-const gulp          = require('gulp');
-const sass          = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+const autoprefixer  = require('gulp-autoprefixer');
+const cleanCSS      = require('gulp-clean-css');
+const sourcemaps    = require('gulp-sourcemaps');
 const browserSync   = require('browser-sync').create();
 
 sass.compiler = require('node-sass');
 
-gulp.task('sass', () => gulp.src('./sass/**/*.scss')
-                            .pipe(sass().on('error', sass.logError))
-                            .pipe(gulp.dest('./css'))
-                            .pipe(browserSync.stream())
+gulp.task('css-build', () => {
+    return gulp.src('./css/*.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+        .pipe(gulp.dest('./css'));
+    }
 );
 
-gulp.task('serve', gulp.series('sass', () => {
+gulp.task('sass-build', () => {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('build', gulp.series('sass-build', 'css-build'));
+
+gulp.task('sass-dev', () => {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('dev', () => {
     browserSync.init({
         server: './',
-        port: 3000
     });
 
-    gulp.watch('./sass/**/*.scss', gulp.series('sass')).on('change', browserSync.reload);
+    gulp.watch('./sass/**/*.scss', gulp.series('sass-dev')).on('change', browserSync.reload);
     gulp.watch('./*.html').on('change', browserSync.reload);
-}));
+});

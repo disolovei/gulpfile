3db23c4a349aed79b1350dbe61f69ed5c9a6ad8c
Dima Solovey <vlnsk@ukr.net>
2020-01-21 12:07:58 +0200

    Add "dev-build-only" task

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,61 @@
-'use strict';
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+"use strict";
+const gulp = require("gulp");
+const sass = require("gulp-sass");
 
-const autoprefixer  = require('gulp-autoprefixer');
-const cleanCSS      = require('gulp-clean-css');
-const sourcemaps    = require('gulp-sourcemaps');
-const browserSync   = require('browser-sync').create();
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const sourcemaps = require("gulp-sourcemaps");
+const browserSync = require("browser-sync").create();
 
-sass.compiler = require('node-sass');
+sass.compiler = require("node-sass");
 
-gulp.task('css-build', () => {
-    return gulp.src('./css/*.css')
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
-        .pipe(gulp.dest('./css'));
-    }
-);
-
-gulp.task('sass-build', () => {
-    return gulp.src('./sass/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./css'));
+gulp.task("css-build", () => {
+    return gulp
+        .src("./css/*.css")
+        .pipe(cleanCSS({ compatibility: "ie8" }))
+        .pipe(autoprefixer("last 2 version", "safari 5", "ie 8", "ie 9"))
+        .pipe(gulp.dest("./css"));
 });
 
-gulp.task('build', gulp.series('sass-build', 'css-build'));
+gulp.task("sass-build", () => {
+    return gulp
+        .src("./sass/**/*.scss")
+        .pipe(sourcemaps.init())
+        .pipe(
+            sass({
+                includePaths: require("node-normalize-scss").includePaths
+            }).on("error", sass.logError)
+        )
+        .pipe(sourcemaps.write("./maps"))
+        .pipe(gulp.dest("./css"));
+});
 
-gulp.task('sass-dev', () => {
-    return gulp.src('./sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'))
+gulp.task("build", gulp.series("sass-build", "css-build"));
+
+gulp.task("sass-dev", () => {
+    return gulp
+        .src("./sass/**/*.scss")
+        .pipe(
+            sass({
+                includePaths: require("node-normalize-scss").includePaths
+            }).on("error", sass.logError)
+        )
+        .pipe(gulp.dest("./css"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('dev', () => {
+gulp.task("dev", () => {
     browserSync.init({
-        server: './',
+        server: "./"
     });
 
-    gulp.watch('./sass/**/*.scss', gulp.series('sass-dev')).on('change', browserSync.reload);
-    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch("./sass/**/*.scss", gulp.series("sass-dev")).on(
+        "change",
+        browserSync.reload
+    );
+    gulp.watch("./*.html").on("change", browserSync.reload);
+});
+
+gulp.task("dev-build-only", () => {
+    gulp.watch("./sass/**/*.scss").on("change", gulp.series("sass-dev"));
 });
